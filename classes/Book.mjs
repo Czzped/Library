63964dafda7200faf7096e0b4b38891dc78e8b8d
Pedro Henrique <phcosta069@gmail.com>
2023-06-10 22:10:44 -0300

    feat: Book class include

---
+++ classes/Book.mjs
@@ -0,0 +1,8 @@
+export class Book {
+    constructor(authorName, publicationYear, numbersOfPages, bookAvailabilityStatus) {
+        this.authorName = authorName
+        this.publicationYear = publicationYear
+        this.numbersOfPages = numbersOfPages
+        this.bookAvailabilityStatus = bookAvailabilityStatus
+    }
+}
